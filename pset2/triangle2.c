9f67e32e802630bed982b1068ebf505956701dc5
Christian Stowers <cstowers77@gmail.com>
2017-11-13 13:41:36 -0500

    Update triangle2.c

--- pset2/triangle2.c
+++ pset2/triangle2.c
@@ -23,7 +23,10 @@ int main(int argc, char * argv[])
 
 string is_triangle(int a, int b, int c)
 {
-
+    if (a <= 0 || b <= 0 || c <= 0)
+    {
+        return "false";   
+    }
     if ((a + b) > c && (b + c) > a && (a + c) > b)
     {
         return "true";
