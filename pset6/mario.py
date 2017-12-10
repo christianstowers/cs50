148adeeae02e70abcf51ea4812676afebb1a401b
Christian Stowers <cstowers77@gmail.com>
2017-12-10 11:22:35 -0500

    Update mario.py

--- pset6/mario.py
+++ pset6/mario.py
@@ -1,18 +1,19 @@
+#now fix this while loop to guarantee a 1-23 height range
 while (True):
     height = int(input("Height: "))
     break
 
 for i in range(height):
     for j in range(height - i - 1):
-        print (" ", end = " ")
+        print (' ', end = '', flush = True)
 
     for i in range(i + 1):
-        print ("#", end = " ")
+        print ('#', end = '', flush = True)
 
-    print ("  ")
+    print (' ', end='')
 
-    for l in range(height - i - 1):
-        print ("#")
+    for l in range(i + 1):
+        print ('#', end= '')
 
-    print ("\n")
+    print ('\r')
 
